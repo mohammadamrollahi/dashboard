192596098228585a7c4e510ddac9f114f967522e
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-04-08 20:38:18 +0430

    add navbar and fixed right menu

---
+++ event.js
@@ -0,0 +1,13 @@
+window.onscroll=function(){myFunc()};
+function myFunc()
+{
+   if( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+   {
+       document.getElementById("navbar").style.top="0";
+
+   }
+   else
+   {
+    document.getElementById("navbar").style.top="-60px" ;
+   }
+}
